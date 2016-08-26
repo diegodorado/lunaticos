68d6d0fa427a9df573f00a8ab68ae6c2bcae2c0d
Diego Dorado <diegodorado@gmail.com>
2016-08-26 18:42:55 -0300

    ordered folders

--- Assets/Demo/Scripts/MeteoroidGrabber.cs
+++ Assets/Demo/Scripts/MeteoroidGrabber.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class MeteoroidGrabber : MonoBehaviour {
     public GameObject[] sounds;
 
+
+    void Awake()
+    {
+        //randomize a little
+        var autoSpin = GetComponent<AutoSpinObject>();
+        transform.position += Vector3.up * Random.Range(-1f, 0.2f);
+        transform.localScale = Vector3.one * Random.Range(0.6f, 1f);
+
+        autoSpin.spinVector = Random.onUnitSphere;
+        autoSpin.speed = Random.Range(10, 60);
+        autoSpin.direction = Random.Range(-1, 1);
+        
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         var player = other.GetComponent<Player>();
